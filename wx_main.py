05bf3aa50531ff36afb7ff5e9524cb965d2d0c49
Ram-N <ramnarasimhan@gmail.com>
2012-12-02 20:46:21 -0800

    temp calcs

--- wx_main.py
+++ wx_main.py
@@ -1,56 +1,91 @@
 #!/usr/bin/env python
 
 import logging
-import oauth2
 import os
-os.system("cls")    #Windows based systems us
 import time
-import httplib
-import urllib
 import sys
-
-import string
 import math
-
-import gflags
-#NLTK related imports
-import re
-from nltk.corpus import wordnet as wn
-from bs4 import BeautifulSoup
-
-from unidecode import unidecode
-from web_util import encode_json, decode_json
-
-import cfg
-from text_utils import *
-from om_utils import *
-from om_client import *
-from webList_to_OM import *
+from collections import defaultdict #automatically initializes to zero
 
 logging.basicConfig(level=logging.DEBUG, filename='wx.log')
 logging.basicConfig(level=logging.INFO, filename='wx.log')
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def  scoredict = read_user_scoreDict():
+def read_user_scoreDict(fi):
   scoredict = {}
-  
-  #format scoredict[minmax] = score
-
+  score = fi.readlines()[1:] #reads one line at a time, skip the header row  
+  for line in score:
+    x = line.split(",")
+    minmax = (int(x[0]),int(x[1]))
+    score = int(x[2])
+    scoredict[minmax] = score
   return scoredict
+
+
+def score_give_tempDict(tD,scoredict):
+  aggscore = 0
+  numh=0
+
+  for h,t in tD.items():
+    score = score_given_temp(t,scoredict)
+    print h, t, score, "(h,temp,score)"
+    aggscore+=score
+    numh+=1
+  return float(aggscore/numh)
+
+def score_given_temp(temp,scoredict):
+  score = 0
+  for tup in scoredict.keys():
+    if temp >= tup[0] and temp < tup[1]:
+        print 'found'
+        score = scoredict[tup]      
+  return score
+
+def   read_daily_temp(fi):
+  dayTempdict = defaultdict(float)
+  hourstotal = defaultdict(float)
+  samples = defaultdict(int)
+  tlines = fi.readlines()[1:] #reads one line at a time, skip the header row  
+  for l in tlines:
+    dte = l.split()[0]
+    hour = l.split()[1].split(",")[0]
+    hr = int(hour.split(":")[0])
+    temp = float(l.split()[1].split(",")[1])
+    print dte, hr, temp
+    samples[hr]+=1
+    hourstotal[hr]+=temp
+
+  for h in samples.keys():
+    print "hr", h, samples[h], hourstotal[h]
+    dayTempdict[h] = (hourstotal[h]/samples[h])
+
+  return dayTempdict
 
 
 if __name__ == '__main__':
 
-  argv = cfg.FLAGS(sys.argv)
-  print argv
-
-  
   # Step 1 Read in the relevant file
   #read the input directory path
-  #rawDirPath = r'C:\Users\Ram\Root-1\OM-API-Utilities\ListsToBeCreated'
 
   #dirList=os.listdir(rawDirPath) #list of filenames
   # ##########################
-  
 
-  scoredict = read_user_scoreDict()
+  rawDirPath = r'ra_score.csv'
+  f = open(rawDirPath, 'r')
+  scoredict = read_user_scoreDict(f)
+  f.close()
+
+  rawDirPath = r'KILSCHAU1.csv'
+  f = open(rawDirPath, 'r')
+  dayTempdict = read_daily_temp(f)
+  f.close()
+
+  for h,t in dayTempdict.items():
+    print h, t
+
+  #for (k,v) in scoredict.items():
+  #  print k,v
+
+
+  score =  score_give_tempDict(dayTempdict,scoredict)
+  print score, "agg score"
